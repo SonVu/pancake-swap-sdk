2d88783cc3c8838397886fbd45a11d1d675541e1
sonvu.ae <sonvu.ae@gmail.com>
2022-06-03 10:41:55 +0700

    fix: constants network

---
+++ dist/constants.d.ts
@@ -0,0 +1,46 @@
+import JSBI from 'jsbi';
+export declare type BigintIsh = JSBI | number | string;
+export declare enum ChainId {
+    MAINNET = 56,
+    TESTNET = 97,
+    MUMBAI = 80001
+}
+export declare enum TradeType {
+    EXACT_INPUT = 0,
+    EXACT_OUTPUT = 1
+}
+export declare enum Rounding {
+    ROUND_DOWN = 0,
+    ROUND_HALF_UP = 1,
+    ROUND_UP = 2
+}
+export declare const FACTORY_ADDRESS = "0xA347c3a519EB0B756F018Fe133E17D8884232872";
+export declare const FACTORY_ADDRESS_MAP: {
+    56: string;
+    97: string;
+    80001: string;
+};
+export declare const INIT_CODE_HASH = "0xd2c7bc7bfe7142acc05a254a014ba2bd0bf49cb9db4c221ab418b1db2efa9f25";
+export declare const INIT_CODE_HASH_MAP: {
+    56: string;
+    97: string;
+    80001: string;
+};
+export declare const MINIMUM_LIQUIDITY: JSBI;
+export declare const ZERO: JSBI;
+export declare const ONE: JSBI;
+export declare const TWO: JSBI;
+export declare const THREE: JSBI;
+export declare const FIVE: JSBI;
+export declare const TEN: JSBI;
+export declare const _100: JSBI;
+export declare const FEES_NUMERATOR: JSBI;
+export declare const FEES_DENOMINATOR: JSBI;
+export declare enum SolidityType {
+    uint8 = "uint8",
+    uint256 = "uint256"
+}
+export declare const SOLIDITY_TYPE_MAXIMA: {
+    uint8: JSBI;
+    uint256: JSBI;
+};
