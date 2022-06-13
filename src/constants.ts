209b00317570b18319cb1be6140b5f0727d3fd73
sonvu.ae <sonvu.ae@gmail.com>
2022-06-13 11:50:37 +0700

    change router

--- src/constants.ts
+++ src/constants.ts
@@ -6,34 +6,34 @@ export type BigintIsh = JSBI | number | string
 export enum ChainId {
   MAINNET = 56,
   TESTNET = 97,
-  MUMBAI = 80001,
+  MUMBAI = 80001
 }
 
 export enum TradeType {
   EXACT_INPUT,
-  EXACT_OUTPUT,
+  EXACT_OUTPUT
 }
 
 export enum Rounding {
   ROUND_DOWN,
   ROUND_HALF_UP,
-  ROUND_UP,
+  ROUND_UP
 }
 
-export const FACTORY_ADDRESS = '0xA347c3a519EB0B756F018Fe133E17D8884232872'
+export const FACTORY_ADDRESS = '0x1102Fd5C250CBA19195F78FFb0BAf159E05cb01b'
 
 export const FACTORY_ADDRESS_MAP = {
   [ChainId.MAINNET]: FACTORY_ADDRESS,
   [ChainId.TESTNET]: '0x6725f303b657a9451d8ba641348b6761a6cc7a17',
-  [ChainId.MUMBAI]: '0xA347c3a519EB0B756F018Fe133E17D8884232872',
+  [ChainId.MUMBAI]: '0x1102Fd5C250CBA19195F78FFb0BAf159E05cb01b'
 }
 
-export const INIT_CODE_HASH = '0xd2c7bc7bfe7142acc05a254a014ba2bd0bf49cb9db4c221ab418b1db2efa9f25'
+export const INIT_CODE_HASH = '0x5ae65da6662188c2e508cb6bcb59f05483a527fc27e586ef49c48f3ef9443469'
 
 export const INIT_CODE_HASH_MAP = {
   [ChainId.MAINNET]: INIT_CODE_HASH,
   [ChainId.TESTNET]: '0xd0d4c4cd0848c93cb4fd1f498d7013ee6bfb25783ea21593d5834f5d250ece66',
-  [ChainId.MUMBAI]: '0xd2c7bc7bfe7142acc05a254a014ba2bd0bf49cb9db4c221ab418b1db2efa9f25',
+  [ChainId.MUMBAI]: '0x5ae65da6662188c2e508cb6bcb59f05483a527fc27e586ef49c48f3ef9443469'
 }
 
 export const MINIMUM_LIQUIDITY = JSBI.BigInt(1000)
@@ -51,10 +51,10 @@ export const FEES_DENOMINATOR = JSBI.BigInt(10000)
 
 export enum SolidityType {
   uint8 = 'uint8',
-  uint256 = 'uint256',
+  uint256 = 'uint256'
 }
 
 export const SOLIDITY_TYPE_MAXIMA = {
   [SolidityType.uint8]: JSBI.BigInt('0xff'),
-  [SolidityType.uint256]: JSBI.BigInt('0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff'),
+  [SolidityType.uint256]: JSBI.BigInt('0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff')
 }
